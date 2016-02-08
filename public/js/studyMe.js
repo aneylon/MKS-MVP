d2d81ecac981179906d88bd3c4981e7e177f76f7
Arlen Neylon <arlen.m.neylon@gmail.com>
2016-02-08 15:49:29 -0800

    Basic Angular working on index from local file system

--- public/js/studyMe.js
+++ public/js/studyMe.js
@@ -3,3 +3,13 @@ console.log("loggin basic");
 $(document).ready(function(){
   console.log('j-quezy fo sheezy');
 });
+
+var studyMe = angular.module('studyMe', [])
+.controller('studyController', function($scope){
+  $scope.testOne = "Test One";
+  $scope.clickedIt = function(){
+    console.log('Clicked it');
+  };
+});
+// .factory()
+// .service()
