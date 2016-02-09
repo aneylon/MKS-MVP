58950d733c683014872f4fd275fe757367d05d33
Arlen Neylon <arlen.m.neylon@gmail.com>
2016-02-08 16:16:45 -0800

    Refactored server to express, verified serving files on local host.

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-// server.js
-// the most basic of node servers
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 4569;
 
+app.use(express.static(__dirname + '/public'));
+app.listen(port);
+/*
+// probably don't need this...
+app.get("/",function(req,res){
+  res.send('test');
+  // res.render(index);
+});
+*/
+/*
+// crazy simple node server
 var http = require('http');
 var port = process.env.PORT || 4569;
 //process.env.PORT, process.env.IP
@@ -10,3 +22,4 @@ http.createServer(function(req,res){
 }).listen(port);
 
 console.log('server running on : ', port);
+*/
